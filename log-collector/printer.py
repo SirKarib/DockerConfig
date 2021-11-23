e2420f8def66753de50364035be6376ac87d9906
Kirill Chirkov <cyrilchirkov@gmail.com>
2021-11-23 14:59:35 +0400

    Update printer.py

--- log-collector/printer.py
+++ log-collector/printer.py
@@ -1,2 +1,14 @@
+import datetime
+
+
+def print_logs():
+    dt_info = datetime.datetime.today().strftime("%A, %d %B %Y, %H:%M:%S")
+    request_text = "hello"
+    log = dt_info + ' - ' + request_text
+    with open("logs.txt", "a") as file:
+        file.write(log + '\n')
+    file.close()
+
+
 if __name__ == "__main__":
-    print("Log")
+    print_logs()
