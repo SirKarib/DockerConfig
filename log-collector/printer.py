8248cd41748ad18c062053ae3adf04b573a31a4d
Kirill Chirkov <cyrilchirkov@gmail.com>
2021-12-09 22:35:38 +0400

    Update printer.py

--- log-collector/printer.py
+++ log-collector/printer.py
@@ -1,10 +1,10 @@
 import os
 
-log_path = os.getenv("LOG_PATH", "../var/log/web-server/register.log")
+LOG_PATH = os.getenv("LOG_PATH", "../var/log/web-server/register.log")
 
 
 def print_logs():
-    with open(log_path, "r") as file:
+    with open(LOG_PATH, "r") as file:
         for line in file:
             print(line.strip())
     file.close()
